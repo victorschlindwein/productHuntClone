75366438f31ad3b8dc3f92010f1853f45389a4f7
DizzySch <victorwilbert@gmail.com>
2020-04-16 10:00:40 -0300

    finaliza api com paginacao e cors

--- src/controllers/ProductController.js
+++ src/controllers/ProductController.js
@@ -3,7 +3,16 @@ const Product = mongoose.model('Product');
 
 module.exports = {
   async index(req, res){
-    const products = await Product.find();
+    const { page = 1 } = req.query;
+    const products = await Product.paginate(
+    {
+      //where
+    },
+    {
+      page,
+      limit: 10
+    }
+    );
 
     return res.json(products);
   },
